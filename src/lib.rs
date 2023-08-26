b62b7e694b7e675db6553420ab902320ee4df2aa
Chris Lunsford <chris.lunsford@fox.com>
2023-08-26 00:59:57 -0400

    Allow configuration via environment variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
 use chrono::{DateTime, Utc};
-use config::Config;
+use config::{Config, Environment};
 use ipnetwork::{IpNetwork, Ipv4Network, Ipv6Network};
 use reqwest;
 use serde::{Deserialize, Serialize};
@@ -18,19 +18,15 @@ use std::rc::Rc;
 lazy_static! {
     // static ref AWS_IP_RANGES_CONFIG_BUILDER: ConfigBuilder<DefaultState> =
    static ref AWS_IP_RANGES_CONFIG: Config = {
+        let home_dir = dirs::home_dir().unwrap();
+        let cache_file: PathBuf = [&home_dir.to_str().unwrap(), ".aws", "ip-ranges.json"].iter().collect();
+
         let config_builder = Config::builder()
-        .set_default("url", "https://ip-ranges.amazonaws.com/ip-ranges.json").unwrap()
-        .set_default(
-            "cache_file",
-            [
-                dirs::home_dir().unwrap().to_str().unwrap(),
-                ".aws",
-                "ip-ranges.json",
-            ]
-            .iter()
-            .collect::<PathBuf>().to_str(),
-        ).unwrap()
-        .set_default("cache_time", 24 * 60 * 60).unwrap();
+            .set_default("url", "https://ip-ranges.amazonaws.com/ip-ranges.json").unwrap()
+            .set_default("cache_file", cache_file.to_str()).unwrap()
+            .set_default("cache_time", 24 * 60 * 60).unwrap()
+            .add_source(Environment::with_prefix("AWS_IP_RANGES"));
+
         config_builder.build().unwrap()
    };
 }
